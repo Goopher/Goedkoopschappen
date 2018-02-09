4642d44bfe8badae3a2a5efb3b00af06a714c3e8
Goopher <goopher@live.nl>
2018-02-09 15:55:09 +0100

    Update data.service.ts

--- Frontend/Goedkoopschappen/src/app/services/data.service.ts
+++ Frontend/Goedkoopschappen/src/app/services/data.service.ts
@@ -5,22 +5,5 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class DataService {
 
-  constructor(private http:Http) {
-    console.log('Data service connected...');
 
-   }
-
-   getProducts(searchString){
-      return this.http.get('http://81.206.45.176:50000/products', {params: {"productName": searchString} })
-      .map(res => res.json());
-   }
-
-   addProductToCart(product) {
-     console.log(product);
-     const headers = new Headers();
-     headers.append('Content-type', 'application/json');
-     const options = new RequestOptions({headers: headers});
-     return this.http.post("http://81.206.45.176:50000/addToCart", JSON.stringify(product), options)
-     .pipe();
-   }
 }
