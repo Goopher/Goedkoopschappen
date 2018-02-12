0aede960df2c3cbba88eb3d0ef2ab69fe45538d2
Grasscropper <i.shenhav@gmail.com>
2018-02-12 10:44:15 +0100

    test2

--- Frontend/Goedkoopschappen/src/app/components/product/product.component.ts
+++ Frontend/Goedkoopschappen/src/app/components/product/product.component.ts
@@ -41,7 +41,7 @@ export class ProductComponent implements OnInit {
 
 }
 
-interface Product {
+export class Product {
   product_name:string;
   product_brand:string;
   description:string;
--- Frontend/Goedkoopschappen/src/app/services/data.service.ts
+++ Frontend/Goedkoopschappen/src/app/services/data.service.ts
@@ -1,9 +1,25 @@
 import { Injectable } from '@angular/core';
-import { Http, Headers, RequestOptions } from '@angular/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import 'rxjs/add/operator/map';
+import { Product } from '../components/product/product.component';
+import { Observable } from 'rxjs/Observable';
 
 @Injectable()
 export class DataService {
+  headers;
+  constructor(private http:HttpClient) {
+    console.log('Data service connected...');
+    this.headers = new HttpHeaders();
+    this.headers.append('Content-type', 'application/json');
+   }
 
+   getProducts(searchString): Observable<Product[]>{
+      return this.http.get<Product[]>('http://localhost:8080/products', {params: {"productName": searchString} });
+   }
 
+   addProductToCart(product) {
+     console.log(product);
+     return this.http.post("http://localhost:8080/addToCart", JSON.stringify(product), this.headers)
+     .pipe();
+   }
 }
